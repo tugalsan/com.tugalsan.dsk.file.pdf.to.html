6e760d8a3ab083a3eefd82ccfd3e2b67a1894f80
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-07-18 09:31:20 +0300

    .

--- run_gui.bat
+++ run_gui.bat
@@ -1,1 +1,1 @@
-java -jar target\com.tugalsan.dsk.file.pdf.to.html-1.0-SNAPSHOT-jar-with-dependencies_cleaned-and-migrated.jar
+java -jar target\com.tugalsan.dsk.file.pdf.to.html-1.0-SNAPSHOT-jar-with-dependencies.jar
