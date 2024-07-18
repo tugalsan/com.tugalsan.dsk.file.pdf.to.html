6750b5ac9074aca498a1065db04f6744008a08f5
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-07-18 09:27:29 +0300

    .

--- src/main/java/com/tugalsan/dsk/file/pdf/to/html/MainWork.java
+++ src/main/java/com/tugalsan/dsk/file/pdf/to/html/MainWork.java
@@ -1,5 +1,6 @@
 package com.tugalsan.dsk.file.pdf.to.html;
 
+import com.tugalsan.api.file.server.TS_FileUtils;
 import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.unsafe.client.TGS_UnSafe;
 import java.io.PrintWriter;
@@ -14,6 +15,11 @@ public class MainWork {
 
     public static void work(boolean isConsole, Path srcPDF, Path dstHTM) {
         TGS_UnSafe.run(() -> {
+            TS_FileUtils.deleteFileIfExists(dstHTM);
+            if (TS_FileUtils.isExistFile(dstHTM)) {
+                d.cr("work", "ERROR canot delete outputFile", dstHTM);
+                System.exit(1);
+            }
             d.cr("work", "init", srcPDF, dstHTM);
             try (var pdf = PDDocument.load(srcPDF.toFile()); var output = new PrintWriter(dstHTM.toFile(), StandardCharsets.UTF_8);) {
                 new PDFDomTree().writeText(pdf, output);
