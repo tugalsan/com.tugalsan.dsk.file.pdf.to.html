7727209924c2ca07a1c197e5562e7be3805e01d1
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-07-18 09:06:46 +0300

    .

--- src/main/java/com/tugalsan/dsk/file/pdf/to/html/Main.java
+++ src/main/java/com/tugalsan/dsk/file/pdf/to/html/Main.java
@@ -1,43 +1,147 @@
 package com.tugalsan.dsk.file.pdf.to.html;
 
+import com.tugalsan.api.desktop.server.TS_DesktopDialogInfoUtils;
+import com.tugalsan.api.desktop.server.TS_DesktopJMenuButton;
+import com.tugalsan.api.desktop.server.TS_DesktopJMenuButtonBar;
 import com.tugalsan.api.desktop.server.TS_DesktopMainUtils;
+import com.tugalsan.api.desktop.server.TS_DesktopPathUtils;
 import com.tugalsan.api.desktop.server.TS_DesktopWindowAndFrameUtils;
+import com.tugalsan.api.file.properties.server.TS_FilePropertiesUtils;
+import com.tugalsan.api.file.server.TS_FileUtils;
+import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
+import com.tugalsan.api.unsafe.client.TGS_UnSafe;
+import com.tugalsan.lib.file.pdf.to.html.server.TS_LibFilePdfFromToUtils;
 import java.awt.Component;
-import javax.swing.JFrame;
-import javax.swing.JMenuBar;
-import javax.swing.JPanel;
+import java.nio.file.Path;
+import java.util.Properties;
+import java.util.stream.IntStream;
+import javax.swing.WindowConstants;
 
 public class Main {
 
-    //cd C:\me\codes\com.tugalsan\trm\com.tugalsan.dsk.file.pdf.to.html
+    final private static TS_Log d = TS_Log.of(Main.class);
+
+    final public static Path pathCurDir = TS_DesktopPathUtils.currentFolder();
+    public static Path pathConfig = pathCurDir.resolve(Main.class.getPackageName() + ".properties");
+
+    public static Properties props;
+    public static MainFrame mainFrame;
+
+    //cd C:\git\dsk\com.tugalsan.dsk.file.pdf.to.html
     //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.dsk.file.pdf.to.html-1.0-SNAPSHOT-jar-with-dependencies.jar
     public static void main(String[] args) {
-        TS_DesktopMainUtils.setThemeAndinvokeLaterAndFixTheme(() -> makeComponent(TS_ThreadSyncTrigger.of(), args));
+        TGS_UnSafe.run(() -> {
+            var kt = TS_ThreadSyncTrigger.of();
+            var configLoadCmdFlagIdx = IntStream.range(0, args.length)
+                    .filter(i -> args[i].equals(TS_LibFilePdfFromToUtils.EXECUTE_PARAM_LOAD_CONFIG_FILE)).findAny().orElse(-1);
+            if (configLoadCmdFlagIdx == -1) {
+                gui(kt);
+            } else {
+                console(kt, args, configLoadCmdFlagIdx);
+            }
+        }, e -> d.ct("main", e));
     }
 
-    public static Component makeComponent(TS_ThreadSyncTrigger kt, String[] args) {
-        var frame = new JFrame();
-        TS_DesktopWindowAndFrameUtils.exitOnClose(frame);
-        TS_DesktopWindowAndFrameUtils.setBorder(frame);
-//        TS_DesktopWindowAndFrameUtils.setLoc(frame, new (10, 10, 1000, 700));
-//        TS_DesktopWindowAndFrameUtils.setfavIcon(frame, TS_DesktopResourceUtils.imageIcon("/images/favicons/screen.png").getImage());
-        TS_DesktopWindowAndFrameUtils.setContent(frame, createPanel());
-        TS_DesktopWindowAndFrameUtils.setTitleSizeCenterWithMenuBar(frame, Main.class.getPackageName(), createMenuBar());
-        frame.setVisible(true);
-        return frame;
+    private static void console(TS_ThreadSyncTrigger kt, String[] args, int configLoadCmdFlagIdx) {
+        if (args.length <= configLoadCmdFlagIdx + 1) {
+            d.ce("main", "ERROR: config file is not given!");
+            System.exit(1);
+        }
+        pathConfig = Path.of(args[configLoadCmdFlagIdx + 1]);
+        if (!TS_FileUtils.isExistFile(pathConfig)) {
+            d.ce("main", "ERROR: config file is not found!", pathConfig);
+            System.exit(1);
+        }
+        Path pathInput = null;
+        { //READ PROPERTIES
+            d.cr("console", "props.reading...");
+            var u_props = TS_FilePropertiesUtils.createPropertyReader(pathConfig);
+            if (u_props.isExcuse()) {
+                d.ce("console", "ERROR: " + u_props.excuse().getMessage());
+                props = TS_FilePropertiesUtils.empty();
+                System.exit(1);
+            }
+            d.cr("console", "props.read: OK");
+            props = u_props.value();
+            {
+                var op = TS_FilePropertiesUtils.getValue(props, TS_LibFilePdfFromToUtils.CONFIG_PARAM_PATH_INPUT);
+                if (op.isEmpty()) {
+                    d.ce("console", "ERROR: " + TS_LibFilePdfFromToUtils.CONFIG_PARAM_PATH_INPUT + " param not present in config file");
+                    System.exit(1);
+                }
+                var str = op.orElseThrow();
+                pathInput = Path.of(str);
+            }
+        }
+        d.cr("console", "pathInput", pathInput);
+        if (!TS_FileUtils.isExistFile(pathInput)) {
+            d.ce("console", "ERROR: pathInput is not found!", pathInput);
+            System.exit(1);
+        }
+        MainWork.work(true, pathInput, TS_LibFilePdfFromToUtils.pathOutput(pathInput));
     }
 
-    public static JMenuBar createMenuBar() {
-        var menuBar = new JMenuBar();
-
-        return menuBar;
+    private static void gui(TS_ThreadSyncTrigger kt) {
+        TGS_UnSafe.run(() -> {
+            TS_DesktopMainUtils.setThemeAndinvokeLaterAndFixTheme(() -> {
+                return gui_component(kt);
+            });
+        }, e -> TS_DesktopDialogInfoUtils.show("error", e.toString()));
     }
 
-    public static JPanel createPanel() {
-        var panel = new JPanel();
-        panel.setSize(300, 300);
-        return panel;
-    }
+    private static Component gui_component(TS_ThreadSyncTrigger kt) {
+        {//CREATE FRAME
+            mainFrame = new MainFrame();
+            mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            TS_DesktopWindowAndFrameUtils.showAlwaysInTop(mainFrame, false);
+        }
 
+        {//LOG LOCATION
+            MainLog.add("curDir: " + pathCurDir);
+            MainLog.add("pathProperties: " + pathConfig);
+        }
+
+        { //READ PROPERTIES
+            MainLog.add("props.reading...");
+            var u_props = TS_FilePropertiesUtils.createPropertyReader(pathConfig);
+            if (u_props.isExcuse()) {
+                MainLog.add("ERROR: " + u_props.excuse().getMessage());
+                props = TS_FilePropertiesUtils.empty();
+            } else {
+                MainLog.add("props.read: OK");
+                props = u_props.value();
+                {
+                    var u = TS_FilePropertiesUtils.getValue(props, TS_LibFilePdfFromToUtils.CONFIG_PARAM_PATH_INPUT);
+                    if (u.isPresent()) {
+                        mainFrame.tfPdfInput.setText(u.orElseThrow());
+                        mainFrame.tfHtmlOutput.setText(
+                                TS_LibFilePdfFromToUtils.pathOutput(
+                                        Path.of(
+                                                mainFrame.tfPdfInput.getText()
+                                        )
+                                ).toAbsolutePath().toString()
+                        );
+                    }
+                }
+            }
+        }
+
+        //INIT FRAME
+        MainPopup.selectAll_copy_togather(mainFrame.tfPdfInput);
+        MainPopup.selectAll_copy_togather(mainFrame.tfHtmlOutput);
+
+        TS_DesktopWindowAndFrameUtils.setTitleSizeCenterWithMenuBar(mainFrame, Main.class.getPackageName(), TS_DesktopJMenuButtonBar.of(
+                TS_DesktopJMenuButton.of("Exit", mx -> {
+                    System.exit(0);
+                }),
+                TS_DesktopJMenuButton.of("About", mx -> {
+                    MainAsync.run(() -> {
+                        TS_DesktopDialogInfoUtils.show("About", "Written by Tuğasan Karabacak (tugalsan@gmail.com)\nFor mesametal.cloud and mebosa.cloud");
+                    });
+                })
+        ));
+        mainFrame.setSize(1000, 690);
+        return mainFrame;
+    }
 }
---
+++ src/main/java/com/tugalsan/dsk/file/pdf/to/html/MainRecordFile.java
@@ -0,0 +1,45 @@
+package com.tugalsan.dsk.file.pdf.to.html;
+
+import com.tugalsan.api.charset.client.TGS_CharSetCast;
+import com.tugalsan.api.file.properties.server.TS_FilePropertiesUtils;
+import static com.tugalsan.dsk.file.pdf.to.html.Main.mainFrame;
+import com.tugalsan.lib.file.pdf.to.html.server.TS_LibFilePdfFromToUtils;
+import java.awt.FileDialog;
+import java.nio.file.Path;
+import javax.swing.text.JTextComponent;
+
+public class MainRecordFile {
+
+    public static Path pathLast = Main.pathCurDir;
+
+    private static void thing(String param, String filter, JTextComponent tf) {
+        MainAsync.run(() -> {
+            Main.mainFrame.taConsole.setText("MainRecordFile.thing." + param + "." + tf.getText());
+            var fd = new FileDialog(Main.mainFrame, TGS_CharSetCast.turkish().toUpperCase(param), FileDialog.LOAD);
+            fd.setDirectory(pathLast.toAbsolutePath().toString());
+            fd.setFile(filter);
+            fd.setVisible(true);
+            var selectedFile = fd.getFile();
+            if (selectedFile == null) {
+                MainLog.add(param + ".cancelled");
+                return;
+            }
+            selectedFile = fd.getDirectory() + selectedFile;
+            MainLog.add(param + ".selected_as:" + selectedFile);
+            tf.setText(selectedFile);
+            TS_FilePropertiesUtils.setValue(Main.props, param, selectedFile);
+            TS_FilePropertiesUtils.write(Main.props, Main.pathConfig);
+        });
+    }
+
+    public static void pdfInput() {
+        thing(TS_LibFilePdfFromToUtils.CONFIG_PARAM_PATH_INPUT, "*.pdf", Main.mainFrame.tfPdfInput);
+        mainFrame.tfHtmlOutput.setText(
+                TS_LibFilePdfFromToUtils.pathOutput(
+                        Path.of(
+                                mainFrame.tfPdfInput.getText()
+                        )
+                ).toAbsolutePath().toString()
+        );
+    }
+}
