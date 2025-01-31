f231c691906fbe2c43bdbdc475456796905f4492
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-01-31 08:50:41 +0300

    Update MainWork.java

--- src/main/java/com/tugalsan/dsk/file/pdf/to/html/MainWork.java
+++ src/main/java/com/tugalsan/dsk/file/pdf/to/html/MainWork.java
@@ -39,40 +39,4 @@ public class MainWork {
         });
     }
 }
-//    @Deprecated //NOT WORKING ERROR: org.apache.pdfbox version incompatible
-//    public static Path castFromPDFtoHTM(Path srcPDF, Path dstHTM, CharSequence optionalTitle, CharSequence optionalHeaderContent, CharSequence optional_iframe_video, boolean addLoader) {
-//        d.ci("castFromPDFtoHTM", srcPDF, dstHTM);
-//        TGS_UnSafe.run(() -> {
-//            castFromPDFtoHTM_do(srcPDF, dstHTM);
-//        }, e -> {
-//            TS_FileTxtUtils.toFile("""
-//            <html><head><title>ERROR</title></head><body>
-//            PDF'den HTM ön izlene dosyası oluşturuken bir hata oluştu. Lütfen orjinal pdf dosyayı indiriniz.<br>
-//            An error occured creating HTM preview file from PDF. Please download the original pdf file.<br>
-//            <br>
-//            %s
-//            </body></html>
-//            """.formatted("ERROR: org.apache.pdfbox version incompatible; disabled until further notice!"), dstHTM, false);
-//            e.printStackTrace();
-//        });
-//
-//        var strHtm = TS_FileTxtUtils.toString(dstHTM);
-//        if (addLoader) {
-//            strHtm = TS_FileHtmlUtils.addLoader(strHtm);
-//        }
-//        if (optional_iframe_video
-//                != null) {
-//            strHtm = TS_FileHtmlUtils.appendResponsiveVideo(strHtm, optional_iframe_video);
-//        }
-//        if (optionalHeaderContent
-//                != null) {
-//            strHtm = TS_FileHtmlUtils.appendToBodyStartAfter(strHtm, optionalHeaderContent);
-//        }
-//        if (optionalTitle
-//                != null) {
-//            strHtm = TS_FileHtmlUtils.updateTitleContent(strHtm, optionalTitle);
-//        }
-//
-//        return TS_FileTxtUtils.toFile(strHtm, dstHTM, false);
-//    }
 
