d59ba9de63e1fb563bd5333260984f786dcb88ae
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-03-10 01:03:44 +0300

    Update MainAsync.java

--- src/main/java/com/tugalsan/dsk/file/pdf/to/html/MainAsync.java
+++ src/main/java/com/tugalsan/dsk/file/pdf/to/html/MainAsync.java
@@ -4,18 +4,20 @@ import com.tugalsan.api.thread.server.async.run.TS_ThreadAsyncRun;
 import .client.TGS_FuncMTUCE;
 import com.tugalsan.api.function.client.maythrow.uncheckedexceptions.TGS_FuncMTUCE;
 import com.tugalsan.api.function.client.maythrow.checkedexceptions.TGS_FuncMTCEUtils;
+import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
 import java.time.Duration;
 
 public class MainAsync {
 
     final public static Duration RUN_SECONDS = Duration.ofSeconds(10);
+    final private static TS_ThreadSyncTrigger killTrigger  = TS_ThreadSyncTrigger.of(MainAsync.class.getSimpleName());
 
     public static void run(TGS_FuncMTUCE exe) {
         run(exe, RUN_SECONDS);
     }
 
     public static void run(TGS_FuncMTUCE exe, Duration until) {
-        TS_ThreadAsyncRun.now(null, kt -> {
+        TS_ThreadAsyncRun.now(killTrigger, kt -> {
 //            var u = TS_ThreadAsyncAwait.runUntil(kt, until, kt2 -> {
 //                MainLog.add("AWAIT.BEGIN...");
                 TGS_FuncMTCEUtils.run(exe, e -> MainLog.add("ERROR: " + e.getMessage()));
